1718a6483e267ddf40b5a0eb4b1bdae7177ff624
Richards-Richie <danderichards360@gmail.com>
2022-11-18 18:37:03 +0530

    completed patterns

--- nestedloop.java
+++ nestedloop.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class nestedloop {
-    public Scanner s=new Scanner(System.in);
+    public static Scanner s=new Scanner(System.in);
     public static void main(String[] args){
         pattern1();
         pattern2();
@@ -12,6 +12,11 @@ public class nestedloop {
         pattern7();
         pattern8();
         pattern9();
+        pattern5_1();
+        patter6_1();
+        patter9_2();
+        pattern10();
+        pattern11();
     }
     public static void pattern1(){
         System.out.println("pattern1");
@@ -142,6 +147,95 @@ public class nestedloop {
                     System.out.print(" 0 ");
                     b=true;
                 }
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern5_1(){
+        System.out.println("pattern 5.1");
+        for(int i=0;i<4;i++){
+            for(int j=0;j<5;j++){
+                if(i == 0 || i == 3 || j==0 || j==4  ){
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void patter6_1(){
+        System.out.println("pattern 6.1");
+        System.out.println("enter rows and cols");
+        int rows=s.nextInt();
+        int cols=s.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=cols-1;j>=i;j--){
+                System.out.print(" - ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public static void patter9_2(){
+        boolean b=true;
+        for(int i=1;i<=5;i++){
+            if(i%2 != 0){
+                b=true;
+            }
+            for(int j=1;j<=i;j++){
+                if(b){
+                    System.out.print(" 1 ");
+                    b=false;
+                }else{
+                    System.out.print(" 0 ");
+                    b=true;
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern10(){
+        System.out.println("pattern 10");
+        int n=5;
+        int rows=6;
+        for(int i=0;i<rows/2;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" * ");
+            }
+            for(int j=0;j<n-1;j++){
+                System.out.print("   ");
+            }
+            n-=2;
+            for(int j=0;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<rows/2;i++){
+            for(int j=3;j>i;j--){
+                System.out.print(" * ");
+            }
+            for(int j=0;j<=n;j++){
+                System.out.print("   ");
+            }
+            n+=2;
+            for(int j=3;j>i;j--){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern11(){
+        for(int i=0;i<5;i++){
+            for(int j=4;j>=i+1;j--){
+                System.out.print("   ");
+            }
+            
+            for(int j=0;j<5;j++){
+                System.out.print(" * ");
             }
             System.out.println();
         }
