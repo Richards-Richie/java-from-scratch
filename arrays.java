60214ad383e5d99ea0ea466c3e6a4480bf96e804
Richards-Richie <danderichards360@gmail.com>
2022-11-24 14:44:08 +0530

    starting with arrays

--- arrays.java
+++ arrays.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class arrays {
     public static void main(String []args){
+        //creatin arry in different ways
+        int []names=new int [10];
+        //int []names1={1,2,3,4,5};
+        //String []names2={"Richards","Indra","Sai"};
+
+
+        //inserting values to arrays 
+        Scanner s=new Scanner(System.in);
+        names[0]=1;
+        names[1]=s.nextInt();
+
+        //output from the array
+        System.out.println(" arrray names is "+names[0]);
+        System.out.println(" arrray names is "+names[1]);
+
+        //passing array as an arguement
         
     }
 }
