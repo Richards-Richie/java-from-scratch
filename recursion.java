49ef90373d902ed13c638561e8486057194250c4
Richards-Richie <danderichards360@gmail.com>
2022-12-12 17:58:49 +0530

    have to complete practise problems

--- recursion.java
+++ recursion.java
@@ -16,6 +16,15 @@ public class recursion {
         System.out.println(power(2,10));
         System.out.println(powerInLogn(2,10));
         System.out.println(tilingProblem(5));
+        StringBuilder sb=new StringBuilder();
+        boolean []b=new boolean[26];
+        removeDuplicates("rich",0,sb,b);
+        System.out.println(noOfWays(3));
+        System.out.println(binaryNumber(3,"",0));
+        recursiveOccurances(arr,0,6 );
+        System.out.println();
+        printNumber(2019);
+        System.out.println(stringLength("richards",0));
     }
     public static void increasingOrder(int n){
         if(n == 1){
@@ -118,4 +127,99 @@ public class recursion {
         int horizontal=tilingProblem(n-2);
         return vertical+horizontal;
     }
+    public static void removeDuplicates(String  str,int index,StringBuilder sb,boolean []map){
+        if(index == str.length()){
+            System.out.println(sb);
+            return;
+        }
+        char c=str.charAt(index);
+        if(map[c-'a'] == true){
+            removeDuplicates(str,index+1,sb,map);
+        }else{
+            map[c-'a']=true;
+            removeDuplicates(str,index+1,sb.append(c),map);
+        }
+    }
+    public static int noOfWays(int n){
+        if(n == 1 || n == 2 || n==0 ){
+            return n;
+        }
+        int singleways=noOfWays(n-1);
+        int pairways=noOfWays(n-2);
+        int noOfPairWays=(n-1)*pairways;
+        return singleways+noOfPairWays;
+    }
+    public static String binaryNumber(int n,String str,int lastplace){
+        if(n == 0){
+            System.out.println(str);
+            return str;
+        }
+        binaryNumber(n-1,str+"0",0);
+        if(lastplace == 0){
+            binaryNumber(n-1,str+"1",1);
+        }
+        return str;
+    }
+    public static void  recursiveOccurances(int []arr,int index,int key){
+        if(index == arr.length){
+            return;
+        }
+        if(arr[index] == key){
+            System.out.print(index+" ");
+            recursiveOccurances(arr,index+1,key);
+
+        }else{
+            recursiveOccurances(arr,index+1,key);
+        }
+    } 
+    public static void printNumber(int n){
+        if( n <= 0){
+            return;
+        }
+        int res=n%10;
+        switch(res){
+            case 0:
+                System.out.print("zero ");
+                break;
+            case 1:
+                System.out.print("one ");
+                break;
+            case 2:
+                System.out.print("two ");
+                break;
+            case 3:
+                System.out.print("three ");
+                break;
+            case 4:
+                System.out.print("four ");
+                break;
+            case 5:
+                System.out.print("five ");
+                break;
+            case 6:
+                System.out.print("six ");
+                break;
+            case 7:
+                System.out.print("seven ");
+                break;
+            case 8:
+                System.out.print("eight ");
+                break;
+            case 9:
+                System.out.print("nine ");
+                break;
+            default :
+                break;
+        }
+        if(n > 0){
+            printNumber(n/10);
+        }
+    }
+    public static int stringLength(String s,int length){
+        if( s.length() == length){
+            return length;
+        }
+        return stringLength(s,length++);
+    }
 }
+
