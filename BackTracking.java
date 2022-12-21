b3472cbd956b4f0d36243302e71fd8c8e95fcaed
Richards-Richie <danderichards360@gmail.com>
2022-12-21 19:03:28 +0530

    completed back tracking

--- BackTracking.java
+++ BackTracking.java
@@ -8,7 +8,7 @@ public class BackTracking {
         subSets("abc","",0);
         System.out.println();
         permutation("abc","");
-        char [][]chessboard=new char[8][8];
+        char [][]chessboard=new char[4][4];
         for(int i=0;i<chessboard.length;i++){
             for(int j=0;j<chessboard.length;j++){
                 chessboard[i][j]='.';
@@ -16,6 +16,31 @@ public class BackTracking {
         }
         nQueens(chessboard,0);
         System.out.println("no of ways to place queens are : "+count);
+        System.out.println(gridWays(0,0,4,4));
+        int [][]sudoku={
+            {0,0,8,0,0,0,0,0,0},
+            {4,9,0,1,5,7,0,0,2},
+            {0,0,3,0,0,4,1,9,0},
+            {1,8,5,0,6,0,0,2,0},
+            {0,0,0,0,2,0,0,6,0},
+            {9,6,0,4,0,5,3,0,0},
+            {0,3,0,0,7,2,0,0,4},
+            {0,4,9,0,3,0,0,5,7},
+            {8,2,7,0,0,9,0,1,3}
+        };
+        if(sudokuSolver(sudoku,0,0)){
+            printSudoku(sudoku);
+        }else{
+            System.out.println("no solution");
+        }
+    }
+    public static void printSudoku(int [][]arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
     public static void printArray(int []arr){
         for(int i=0;i<arr.length;i++){
@@ -102,5 +127,69 @@ public class BackTracking {
             }
             System.out.println();
         }
+    }
+    public static int gridWays(int rows,int cols,int n,int m){
+        //base
+        if(rows == n || cols == m){
+            return 0;
+        }else if(rows == n-1 && cols == m-1){
+            return 1;
+        }
+        //work
+        int a=gridWays(rows+1,cols,n,m);
+        int b=gridWays(rows,cols+1,n,m);
+        return a+b;
+    } 
+    public static boolean sudokuSolver(int [][]sudoku,int rows,int cols){
+        //base case
+        if(rows >8  && cols ==0){
+            return true;
+        }
+        //recursion
+        int nextRow=rows;
+        int nextCol=cols+1;
+        if(nextCol == 9){
+            nextRow=rows+1;
+            nextCol=0;
+        }
+        if(sudoku[rows][cols] !=0){
+            return sudokuSolver(sudoku,nextRow,nextCol);
+        }
+        for(int digit=1;digit <=9;digit++){
+            if(isSafe1(sudoku,rows,cols,digit)){
+                sudoku[rows][cols] = digit;
+                if(sudokuSolver(sudoku,nextRow,nextCol)){
+                    return true;
+                }
+                sudoku[rows][cols] = 0;
+            }
+        }
+        return false;
+    }
+    public static boolean isSafe1(int [][]sudoku,int rows,int cols,int digit){
+        //coloumn
+        for(int i=0;i<=8;i++){
+            if(sudoku[i][cols]== digit){
+                return false;
+            }
+        }
+        //row
+        for(int i=0;i<=8;i++){
+            if(sudoku[rows][i] == digit){
+                return false;
+            }
+        }
+        //grid
+        int startrow=(rows/3)*3;
+        int startcol=(cols/3)*3;
+        
+        for(int i=startrow;i<startrow+3;i++){
+            for(int j=startcol;j<startcol+3;j++){
+                if(sudoku[i][j] == digit){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
