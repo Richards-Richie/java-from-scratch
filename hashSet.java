101955338d06cc19ea8a28dd4d4553ec20da2663
Richards-Richie <danderichards360@gmail.com>
2023-07-18 20:25:22 +0530

    hashsets

--- hashSet.java
+++ hashSet.java
@@ -1,11 +1,36 @@
 import java.util.*;
 public class hashSet {
     public static void main(String[] args) {
-        HashSet <Integer>hs=new HashSet<>();
-        hs.add(1);
-        System.out.println(hs.contains(1));
-        hs.remove(1);
-        System.out.println(hs.contains(1));
-        ;
+        HashSet <ArrayList>hs=new HashSet<>();
+        ArrayList <Integer>ar=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            ar.add(i);
+        }
+        hs.add(ar);
+        //System.out.println(hs);
+        ArrayList <Integer>ar1=new ArrayList<>();
+        for(int i=0;i<5;i++){
+            ar1.add(i);
+        }
+        hs.add(ar1);
+        System.out.println(hs);
+        iterator(hs);
+        TreeSet <Integer>ts=new TreeSet<>();
+        for(int i=10;i>=0;i--){
+            System.out.print(i+" ");
+            ts.add(i);
+        }
+        System.out.println(ts);
+    }
+    public static void iterator(HashSet <ArrayList>hs){
+        System.out.println("iterator");
+        Iterator i=hs.iterator();
+        while(i.hasNext()){
+            System.out.println(i.next());
+        }
+        //for each
+        for(ArrayList <Integer>j: hs){
+            System.out.println(j);
+        }
     }
 }
