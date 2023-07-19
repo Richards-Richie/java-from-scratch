a82d39df234ea8de023e66b8c47161625d325dfd
Richards-Richie <danderichards360@gmail.com>
2023-07-19 16:17:22 +0530

    itenary problem

--- hashSet.java
+++ hashSet.java
@@ -14,13 +14,27 @@ public class hashSet {
         }
         hs.add(ar1);
         System.out.println(hs);
-        iterator(hs);
+        //iterator(hs);
         TreeSet <Integer>ts=new TreeSet<>();
         for(int i=10;i>=0;i--){
             System.out.print(i+" ");
             ts.add(i);
         }
         System.out.println(ts);
+        int []arr1=new int[5];
+        arr1[0]=1;
+        arr1[1]=1;
+        arr1[2]=1;
+        arr1[3]=1;
+        arr1[4]=1;
+        int []arr2=new int[5];
+        arr2[0]=2;
+        arr2[1]=2;
+        arr2[2]=2;
+        arr2[3]=2;
+        arr2[4]=2;
+        //union(arr1,arr2);
+        itenary();
     }
     public static void iterator(HashSet <ArrayList>hs){
         System.out.println("iterator");
@@ -31,6 +45,44 @@ public class hashSet {
         //for each
         for(ArrayList <Integer>j: hs){
             System.out.println(j);
+        }
+    }
+    public static void union(int []arr1,int []arr2){
+        HashSet <Integer>hs=new HashSet<>();
+        int j;
+        boolean b=true;
+        for(int i=0;i<arr1.length;i++){
+            hs.add(arr1[i]);
+        }
+        for(int i=0;i<arr2.length;i++){
+            hs.add(arr2[i]);
+        }
+        System.out.println(hs);
+    }
+    public static String startingPoint(HashMap <String,String>hs){
+        HashMap <String,String>revMap=new HashMap<>();
+        for(String to : hs.keySet()){
+            System.out.println(to + " --> "+hs.get(to));
+            revMap.put(hs.get(to),to);
+        }
+        for(String from : hs.keySet()){
+            if(!revMap.containsKey(from)){
+                return from;
+            }
+        }
+        return null;
+    }
+    public static void itenary(){
+        HashMap <String,String>hs=new HashMap<>();
+        hs.put("chennai","banguluru");
+        hs.put("mumbai","delhi");
+        hs.put("goa","chennai");
+        hs.put("delhi","goa");
+        String start=startingPoint(hs);
+        System.out.println(start);
+        for(String s : hs.keySet()){
+            System.out.print(start+" --> "+hs.get(start)+" ");
+            start=hs.get(start);
         }
     }
 }
