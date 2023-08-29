85eb638410056eff0b98a0bc7ba31898dd1c5fa7
Richards-Richie <danderichards360@gmail.com>
2023-08-30 00:01:40 +0530

    search in bst

--- BinarySearchTrees.java
+++ BinarySearchTrees.java
@@ -28,27 +28,48 @@ public class BinarySearchTrees {
         System.out.println(root.val+" ");
         inorder(root.right);
     }
-    public static Node search(Node root,int key){
+    public static boolean search(Node root,int key){
         if(root == null){
-            return null;
+            return false;
         }
         if(root.val == key){
-            return root;
+            return true;
         }
-        Node l=null;
-        Node r=null;
+        
         if(root.val > key){
-            l=search(root.left,key);
+            return search(root.left,key);
         }
         else{
-            r=search(root.right,key);
+            return search(root.right,key);
         }
-        if(r!=null && r.val ==key){
-            return r;
-        }else if(l!=null && l.val == key){
-            return l;
+    }
+    public static Node delete(Node root,int key){
+        if(key < root.val){
+            return delete(root.left,key);
+        }else if(key > root.val){
+            return delete(root.right,key);
+        }else{
+            if(root.left == null && root.right == null){
+                return null;
+            }
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+            else{
+                Node is=inorderSucessor(root.right);
+                root.val=is.val;
+                root.right=delete(root.right,is.val);
+            }
         }
-        return null;
+        return root;
+    }
+    public static Node inorderSucessor(Node root) {
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
     }
     public static void main(String[] args) {
         int []arr={1,9,2,8,3,7,4,6,5,0};
@@ -56,9 +77,10 @@ public class BinarySearchTrees {
         for(int i=0;i<arr.length;i++){
             root=insert(root,arr[i]);
         }
+        //inorder(root);
+        //System.out.println(search(root, 11));
+        root=delete(root,8);
         inorder(root);
-        System.out.println();
-        System.out.println(search(root, 6).val);
     }
     
 }
