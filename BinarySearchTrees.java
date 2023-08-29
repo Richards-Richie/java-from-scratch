321b1ad9769fecfa4f763ac825cc20b2235ff5f0
Richards-Richie <danderichards360@gmail.com>
2023-08-29 22:30:48 +0530

    search in bst

--- BinarySearchTrees.java
+++ BinarySearchTrees.java
@@ -20,4 +20,45 @@ public class BinarySearchTrees {
         }
         return root;
     }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.val+" ");
+        inorder(root.right);
+    }
+    public static Node search(Node root,int key){
+        if(root == null){
+            return null;
+        }
+        if(root.val == key){
+            return root;
+        }
+        Node l=null;
+        Node r=null;
+        if(root.val > key){
+            l=search(root.left,key);
+        }
+        else{
+            r=search(root.right,key);
+        }
+        if(r!=null && r.val ==key){
+            return r;
+        }else if(l!=null && l.val == key){
+            return l;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        int []arr={1,9,2,8,3,7,4,6,5,0};
+        Node root=null;
+        for(int i=0;i<arr.length;i++){
+            root=insert(root,arr[i]);
+        }
+        inorder(root);
+        System.out.println();
+        System.out.println(search(root, 6).val);
+    }
+    
 }
